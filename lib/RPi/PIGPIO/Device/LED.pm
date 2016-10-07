3ccb8a15f815d5141468089c35e5a8c4a20182a9
Horea Gligan <horea@evozon.com>
2016-10-07 14:14:41 +0300

    Added new generic "Switch" device

--- lib/RPi/PIGPIO/Device/LED.pm
+++ lib/RPi/PIGPIO/Device/LED.pm
@@ -2,11 +2,11 @@ package RPi::PIGPIO::Device::LED;
 
 =head1 NAME
 
-RPi::PIGPIO::Device::LED - Turn on and off a led connected to the RaspberryPi GPIO
+RPi::PIGPIO::Device::LED - Turn on and off a LED
 
 =head1 DESCRIPTION
 
-Uses the pigpio C library turn on and off a led connected to a local or remote RapsberryPi
+Turn on and off a led connected to a local or remote RapsberryPi
 
 What this actually does is set the GPIO to output and allow you to set the levels to HI or LOW
 
@@ -15,8 +15,7 @@ What this actually does is set the GPIO to output and allow you to set the level
 use strict;
 use warnings;
 
-use Carp;
-use RPi::PIGPIO qw/PI_OUTPUT LOW HI PI_CMD_BR1/;
+use base 'RPi::PIGPIO::Device::Switch';
 
 =head1 METHODS
 
@@ -32,30 +31,6 @@ Arguments:
 $pi - an instance of RPi::PIGPIO
 $gpio - GPIO number to which the LED is connected
 
-=cut
-sub new {
-    my ($class,$pi,$gpio) = @_;
-    
-    if (! $gpio) {
-        croak "new() expects the second argument to be the GPIO number to which the LED is connected!";
-    }
-    
-    if (! $pi || ! ref($pi) || ref($pi) ne "RPi::PIGPIO") {
-        croak "new expectes the first argument to be a RPi::PIPGIO object!";
-    }
-    
-    my $self = {
-        pi => $pi,
-        gpio => $gpio,
-        status => undef,
-    };
-    
-    $self->{pi}->set_mode($self->{gpio},PI_OUTPUT);
-    
-    bless $self, $class;
-    
-    return $self;
-}
 
 =head2 on
 
@@ -65,14 +40,6 @@ Usage :
 
     $led->on();
 
-=cut
-sub on {
-    my $self = shift;
-    
-    $self->{pi}->write($self->{gpio},HI);
-}
-
-
 =head2 off
 
 Turn off the led
@@ -81,27 +48,11 @@ Usage :
 
     $led->off();
 
-=cut
-sub off {
-    my $self = shift;
-    
-    $self->{pi}->write($self->{gpio},LOW);
-}
-
-
 =head2 status
 
 Returns the status of the led (checks if the GPIO is set to HI or LOW)
 
 =cut
-sub status {
-    my $self = shift;
-    
-    my $gpio_levels = $self->{pi}->send_command(PI_CMD_BR1, 0, 0);
-    
-    my $status = $gpio_levels & (1<<$self->{gpio});
-    
-    return $status ? 1 : 0;
-}
+
 
 1;
